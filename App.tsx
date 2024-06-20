ed359ae10b83675542515fe3fb4df3a67c193068
hyunho3421 <haha9006@gmail.com>
2024-06-20 16:00:30 +0900

    ''

--- App.tsx
+++ App.tsx
@@ -1,12 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { NavigationContainer } from '@react-navigation/native';
+import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import MainScreen from './screen/MainScreen';
+import HomeScreen1 from './screen/HomeScreen1';
+import HomeScreen2 from './screen/HomeScreen2';
+import DetailsScreen from './screen/Details';
+
+const TopTap = createMaterialTopTabNavigator();
+const Stack = createNativeStackNavigator();
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Main'>
+        <Stack.Screen name='Main' component={MainScreen} />
+        <Stack.Screen name='Home1' component={HomeScreen1} />
+        <Stack.Screen name='Home2' component={HomeScreen2} />
+        <Stack.Screen name='Details' component={DetailsScreen} options={{headerBackVisible: false}}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
